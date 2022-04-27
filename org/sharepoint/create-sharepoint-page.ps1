821688451cf6e7c0d5f9446744f95c78294d98bb
Christian Wallesch <christian.wallesch@glueckkanja-gab.com>
2022-04-27 16:38:03 +0200

    add create sharepoint page file

---
+++ org/sharepoint/create-sharepoint-page.ps1
@@ -0,0 +1,44 @@
+<#
+  .SYNOPSIS
+  Create a shared mailbox.
+
+  .DESCRIPTION
+  Create a shared mailbox.
+
+  .NOTES
+  Permissions given to the Az Automation RunAs Account:
+  AzureAD Roles:
+  - Exchange administrator
+  Office 365 Exchange Online API
+  - Exchange.ManageAsApp
+
+  .INPUTS
+  RunbookCustomization: {
+        "Parameters": {
+            "CallerName": {
+                "Hide": true
+            }
+        }
+    }
+
+#>
+
+#Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }
+
+param (
+    [Parameter(Mandatory = $true)] 
+    [string] $MailboxName,
+    [ValidateScript( { Use-RJInterface -DisplayName "SiteId" } )]
+    [string] $siteId,
+    [ValidateScript( { Use-RJInterface -Type Graph -Entity User -DisplayName "Delegate access to" -Filter "userType eq 'Member'"} )]
+    [string] $DelegateTo,
+    [ValidateScript( { Use-RJInterface -DisplayName "Automatically map mailbox in Outlook" } )]
+    [bool] $AutoMapping = $false,
+    # CallerName is tracked purely for auditing purposes
+    [Parameter(Mandatory = $true)]
+    [string] $CallerName
+)
+
+Connect-RjRbGraph
+
+Invoke-RjRbRestMethodGraph -Resource "/sites/$($siteId)/pages" -Body 
