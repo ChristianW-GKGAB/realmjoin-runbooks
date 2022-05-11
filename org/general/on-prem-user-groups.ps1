fa052ed238b081dcfe2e153b16d08c140bcc5d96
Christian Wallesch <christian.wallesch@glueckkanja-gab.com>
2022-05-11 10:24:00 +0200

    built on prem user group script in server

---
+++ org/general/on-prem-user-groups.ps1
@@ -0,0 +1,31 @@
+import-module ActiveDirectory
+
+#$ADGroupSamAccountNames = @()
+[string] $MigGroupName
+
+
+$ADGroups = @()
+foreach($ADGroupSamAccountName in $ADGroupSamAccountNames){
+    $ADGroups += Get-ADGroup -Identity $ADGroupSamAccountName
+}
+
+$AllUsers = Get-ADUser -Properties whencreated -Filter *
+$beforedate = (Get-Date).AddDays(-1)
+
+$MigGroupMemberGUIDs = get-ADGroupMember -Identity $MigGroupName -Recursive | Select-Object -ExpandProperty ObjectGUID
+$NewUsers = @()
+foreach($User in $AllUsers){
+    if(($User.whencreated -gt $beforedate) -or ($MigGroupMemberGUIDs.Contains($User.ObjectGUID))){
+        $NewUsers += $User
+    }
+}
+foreach($ADGroup in $ADGroups){
+    $ADGroupMembers = @()
+    $ADGroupMembers += get-AdGroupMember -Identity $ADGroup.ObjectGUID -Recursive | Select-Object -ExpandProperty ObjectGUID
+    foreach($NewUser in $NewUsers){
+        if(!($ADgroupMembers.contains($NewUser.ObjectGUID))){
+            Add-ADGroupMember -Identity $ADgroup.ObjectGUID -Members $NewUser.SamAccountName
+        }
+    }
+    
+}
