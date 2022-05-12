8e78b24529283d7b17daac2d909d85d21ec7e75f
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-05-12 13:42:11 +0200

    add export csv capability

--- org/security/on-prem-export-abandoned-devices.ps1
+++ org/security/on-prem-export-abandoned-devices.ps1
@@ -1,79 +1,41 @@
-<#
-  .SYNOPSIS
-  Create a room resource.
+import-module ActiveDirectory
 
-  .DESCRIPTION
-  Create a room resource.
 
-  .NOTES
-  Permissions given to the Az Automation RunAs Account:
-  
+#"Maximum Age for a Password"
+[int] $Days = 30
+#Delete abandoned devices
+[bool] $Delete = $false
+#Disable abandoned devices
+[bool] $Disable = $false
+#export the list of Abandoned Devices in a .csv
+[bool] $exportcsv = $false
+if($exportcsv){
+    #csv Path
+    [string] $CsvPath = 
+    [string] $csvName = "AbandonedDevices" + (get-date).ToString("yyyymmdd")
+}
+#Excluded OU (Organisational Units) 
+[array] $ExcludedOUs = "OUAllowOldPasswords","Domain Controllers"
+# CallerName is tracked purely for auditing purposes
+[string] $CallerName
 
-  .INPUTS
-  RunbookCustomization: {
-        "Parameters": {
-            "Select": {
-                    "Options": [
-                        {
-                            "Display": "Export list of abandoned devices",
-                            "Customization": {
-                                "Default": {
-                                    "Delete": false,
-                                    "Disable": false
-                                }
-                            }
-                        }, {
-                            "Display": "Delete all abandoned devices",
-                            "Customization": {
-                                "Default": {
-                                    "Delete": true
-                                }
-                            }
-                        },
-                        {
-                            "Display": "Disable abandoned devices"
-                            "Customization":{
-                                "Default": {
-                                    "Delete": false,
-                                    "Disable": ture
-                                }
-                            }
-                        }
-                    ]
-                },
-            
-            "CallerName": {
-                "Hide": true
-            }
-        }
-    }
-
-#>
-#Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }, ActiveDirectory
-param (
-    [ValidateScript( { Use-RJInterface -DisplayName "Maximum Age for a Password" } )]
-    [int] $Days = 30,
-    [ValidateScript( { Use-RJInterface -DisplayName "Delete abandoned devices" } )]
-    [bool] $Delete = $false,
-    [ValidateScript( { Use-RJInterface -DisplayName "Disable abandoned devices" } )]
-    [bool] $Disable = $false,
-    [ValidateScript( { Use-RJInterface -DisplayName "Excluded OU (Organisational Units" } )]
-    [String] $ExcludedOU,
-    # CallerName is tracked purely for auditing purposes
-    [Parameter(Mandatory = $true)]
-    [string] $CallerName
-)
 
 $date = [datetime]::Today.AddDays(-$Days)
-$AbandonedDevices = Get-ADComputer -Filter "PasswordLastSet -le $($date)" -Properties * | Where-Object {$_.DistinguishedName -notlike "*OU=$($ExcludedOU),*"}
+
+$AbandonedDevices = Get-ADComputer -Filter 'PasswordLastSet -le $date' -Properties "PasswordLastSet", "DistinguishedName" 
+foreach($ExcludedOU in $ExcludedOUs){
+    $AbandonedDevices = $AbandonedDevices | Where-Object {$_.DistinguishedName -notlike "*OU=$($ExcludedOU),*"}
+}
 
 if($Delete){
     $AbandonedDevices | Remove-ADComputer
 }elseif($Disable){
     foreach ($AbandonedDevice in $AbandonedDevices){
-        set-AdComputer -Identity $AbandonedDevice.
+        set-AdComputer -Identity $AbandonedDevice -Enabled $false
     }
+}elseif($exportcsv){
+    $AbandonedDevices | Export-Csv -Path "$($CsvPath)$($csvName).csv"
 }else{
-    $AbandonedDevices | Export-Csv
+    $AbandonedDevices
 }
 
