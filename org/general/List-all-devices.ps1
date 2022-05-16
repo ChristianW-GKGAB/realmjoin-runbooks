f55c34cc4f1f6f839a5a7359335f7fb310dc5fcf
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-05-16 10:44:53 +0200

    change exported properties

--- org/general/List-all-devices.ps1
+++ org/general/List-all-devices.ps1
@@ -23,25 +23,113 @@
 #Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }
 
 param (
+    [ValidateScript( { Use-RJInterface -Type Setting -Attribute "Devices.Container" } )]
+    [string] $ContainerName,
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( { Use-RJInterface -Type Setting -Attribute "Devices.ResourceGroup" } )]
+    [string] $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( { Use-RJInterface -Type Setting -Attribute "Devices.StorageAccount.Name" } )]
+    [string] $StorageAccountName,
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( { Use-RJInterface -Type Setting -Attribute "Devices.StorageAccount.Location" } )]
+    [string] $StorageAccountLocation,
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( { Use-RJInterface -Type Setting -Attribute "Devices.StorageAccount.Sku" } )]
+    [string] $StorageAccountSku,
     [Parameter(Mandatory = $true)]
     [string] $CallerName
 )
-Connect-RjRbGraph
-$Exportdevices = @()
-$Devices = [psobject]
-$Devices = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices" 
 
-foreach($Device in $Devices){
-    $primaryOwner = Invoke-RjRbRestMethodGraph -Resource "/Users/$($Device.userPrincipalName)" -OdSelect "city, country, department, usageLocation"
-    $Exportdevice = @()
-    $Exportdevice +=$Device
-    $Exportdevice | Add-Member -Name "city" -Value $primaryOwner.city -MemberType "NoteProperty"
-    $Exportdevice | Add-Member -Name "country" -Value $primaryOwner.country -MemberType "NoteProperty"
-    $Exportdevice | Add-Member -Name "department" -Value $primaryOwner.department -MemberType "NoteProperty"
-    $Exportdevice | Add-Member -Name "usageLocation" -Value $primaryOwner.usageLocation -MemberType "NoteProperty"
-    $Exportdevices += $Exportdevice
+if (-not $ContainerName) {
+    $ContainerName = "device-list-" + (get-date -Format "yyyy-MM-dd")
 }
-#zugang zu Az storage um csv zu speichern
-#oder per mail attachment schicken vgl notify-changed-Conditional-Access-Policies.ps1 und notify-changed-CA-policies.ps1
 
-$Exportdevices | ConvertTo-Csv
+Connect-RjRbGraph
+Connect-RjRbAzAccount
+try {
+    # "Getting Process configuration"
+
+    if (-not $ResourceGroupName) {
+        $ResourceGroupName = $processConfig.exportResourceGroupName
+    }
+
+    if (-not $StorageAccountName) {
+        $StorageAccountName = $processConfig.exportStorAccountName
+    }
+
+    if (-not $StorageAccountLocation) {
+        $StorageAccountLocation = $processConfig.exportStorAccountLocation
+    }
+
+    if (-not $StorageAccountSku) {
+        $StorageAccountSku = $processConfig.exportStorAccountSKU
+    }
+
+    if ((-not $ResourceGroupName) -or (-not $StorageAccountName) -or (-not $StorageAccountSku) -or (-not $StorageAccountLocation)) {
+        "## To export to a storage account, please use RJ Runbooks Customization ( https://portal.realmjoin.com/settings/runbooks-customizations ) to specify an Azure Storage Account for upload."
+        "## Alternatively, present values for ResourceGroup and StorageAccount when staring the runbook."
+        ""
+        "## Configure the following attributes:"
+        "## - Devices.ResourceGroup"
+        "## - Devices.StorageAccount.Name"
+        "## - Devices.StorageAccount.Location"
+        "## - Devices.StorageAccount.Sku"
+        ""
+        "## Stopping execution."
+        throw "Missing Storage Account Configuration."
+    }
+
+    $Exportdevices = @()
+    $Devices = [psobject]
+    $Devices = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices" -OdSelect "deviceName, lastSyncDateTime, enrolledDateTime, userPrincipalName, id, serialNumber, manufacturer, model, imei, managedDeviceOwnerType, operatingSystem, osVersion, complianceState"
+
+    foreach ($Device in $Devices) {
+        $primaryOwner = Invoke-RjRbRestMethodGraph -Resource "/Users/$($Device.userPrincipalName)" -OdSelect "city, country, department, usageLocation"
+        $Exportdevice = @()
+        $Exportdevice += $Device
+        if ($primaryOwner) {
+            $Exportdevice | Add-Member -Name "city" -Value $primaryOwner.city -MemberType "NoteProperty"
+            $Exportdevice | Add-Member -Name "country" -Value $primaryOwner.country -MemberType "NoteProperty"
+            $Exportdevice | Add-Member -Name "department" -Value $primaryOwner.department -MemberType "NoteProperty"
+            $Exportdevice | Add-Member -Name "usageLocation" -Value $primaryOwner.usageLocation -MemberType "NoteProperty"
+        }
+        $Exportdevices += $Exportdevice
+    }
+    #zugang zu Az storage um csv zu speichern
+    #oder per mail attachment schicken vgl notify-changed-Conditional-Access-Policies.ps1 und notify-changed-CA-policies.ps1
+
+    $storAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -ErrorAction SilentlyContinue
+    if (-not $storAccount) {
+        "## Creating Azure Storage Account $($StorageAccountName)"
+        $storAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -Location $StorageAccountLocation -SkuName $StorageAccountSku 
+    }
+ 
+    # Get access to the Storage Account
+    $keys = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
+    $context = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $keys[0].Value
+
+    # Make sure, container exists
+    $container = Get-AzStorageContainer -Name $ContainerName -Context $context -ErrorAction SilentlyContinue
+    if (-not $container) {
+        "## Creating Azure Storage Account Container $($ContainerName)"
+        $container = New-AzStorageContainer -Name $ContainerName -Context $context 
+    }
+
+
+    $Exportdevices | ConvertTo-Csv > device.csv
+
+    # Upload
+    Set-AzStorageBlobContent -File "device.csv" -Container $ContainerName -Blob "device.csv" -Context $context -Force | Out-Null
+
+    $EndTime = (Get-Date).AddDays(6)
+    $SASLink = New-AzStorageBlobSASToken -Permission "r" -Container $ContainerName -Context $context -Blob "device.csv" -FullUri -ExpiryTime $EndTime
+
+    "## App Owner/User List Export created."
+    "## Expiry of Link: $EndTime"
+    $SASLink | Out-String
+
+}
+catch {
+    $_
+}
