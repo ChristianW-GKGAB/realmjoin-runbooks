11d39aed8c9de259d3bd3617764dda945dc24171
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-05-17 09:00:55 +0200

    remove ids

--- org/general/add-users-to-groups.ps1
+++ org/general/add-users-to-groups.ps1
@@ -22,40 +22,31 @@
 #>
 #Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }
 
+
 param(
     [Parameter(Mandatory = $true)]
-    [string] $CallerName = "cloudadmin@c4a8cscilla.onmicrosoft.com"
+    [string] $CallerName 
 )
 Connect-RjRbGraph
 
-
-
-
-
 #define TargetgroupIds and MigGroupId variables beforehand
-#$ADGroupSamAccountNames = @()
-[string] $MigGroupId = "7082a9d8-e27f-4ece-840d-32592f5b5967"
-$TargetGroupIds = @("a5df3c1c-f2a1-4485-9489-57f4bf97d4c7", "77d44d2b-0dbf-44dc-95f2-56ada371cf54")
+[string] $MigGroupId = ""
+$TargetGroupIds = @()
 $beforedate = (Get-Date).AddDays(-1) | Get-Date -Format "yyyy-MM-dd"
 
 $AADGroups = @()
 foreach ($TargetGroupId in $TargetgroupIds) {
     $AADGroups += Invoke-RjRbRestMethodGraph -Resource "/groups/$TargetGroupId" 
 }
+
 $filter = 'createdDateTime ge ' + $beforedate + 'T00:00:00Z'
-
 $NewUsers = Invoke-RjRbRestMethodGraph -Resource "/users" -OdFilter $filter
-
 
 $MigGroupMembers = Invoke-RjRbRestMethodGraph -Resource "/groups/$MigGroupId/members"
 $MigrationUsers = @()
 $MigrationUsers += $MigGroupMembers
 $MigrationUsers += $NewUsers
-<#foreach($User in $AllUsers){
-    if($User.createdDateTime -gt $beforedate){
-        $NewUsers += $User
-    }
-}#>
+
 foreach ($AADGroup in $AADGroups) {
     $AADGroupMembers = @()
     $AADGroupMembers += Invoke-RjRbRestMethodGraph -Resource "/groups/$($AADGroup.Id)/members" -OdSelect "Id"
@@ -66,6 +57,5 @@ foreach ($AADGroup in $AADGroups) {
         }
     }
     $GroupJson = @{"members@odata.bind" = $bindings }
-    Invoke-RjRbRestMethodGraph -Resource "/groups/$($AADGroup.Id)" -Method Patch -Body $GroupJson
-    
+    Invoke-RjRbRestMethodGraph -Resource "/groups/$($AADGroup.Id)" -Method Patch -Body $GroupJson 
 }
