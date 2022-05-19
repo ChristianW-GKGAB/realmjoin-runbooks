0499b2305cbe7b051b177e9b0bafcf2c822c0276
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-05-19 14:20:49 +0200

    created role assignment query

--- user/security/list-expiring-role-assignments.ps1
+++ user/security/list-expiring-role-assignments.ps1
@@ -7,8 +7,6 @@
 
   .NOTES
   Permissions: MS Graph
-  - User.Read.All
-  - AuditLogs.Read.All
   - Organization.Read.All
 
   .INPUTS
@@ -26,7 +24,7 @@
 
 param(
   [ValidateScript( { Use-RJInterface -DisplayName "Days without signin" } )]
-  [int] $Days = 30,
+  [int] $Days = 3000,
   [ValidateScript( { Use-RJInterface -DisplayName "Include users/guests that can not sign in" } )]
   [bool] $showBlockedUsers = $true,
   # CallerName is tracked purely for auditing purposes
@@ -37,31 +35,7 @@ param(
 Connect-RjRbGraph
 
 # Calculate "last sign in date"
-$lastSignInDate = (get-date) - (New-TimeSpan -Days $days) | Get-Date -Format "yyyy-MM-dd"
-$filter = 'signInActivity/lastSignInDateTime le ' + $lastSignInDate + 'T00:00:00Z'
-try {
-  $userObjects = Invoke-RjRbRestMethodGraph -Resource '/users' -OdFilter $filter 
-}
-catch {
-  "## Getting list of users and guests failed. Maybe missing permissions?"
-  ""
-  "## Make sure, the following Graph API permissions are present:"
-  "## - Users.Read.All (API)"
-  "## - AuditLog.Read.All (API)"
-  ""
-  $_
-  throw ("Listing users failed.")
-}
+$expiringDate = (get-date).AddDays($Days) | Get-Date -Format "yyyy-MM-dd"
+$filter = "EndDateTime lt $expiringDate" + "T00:00:00Z"
 
-if (-not $showBlockedUsers) {
-  $userObjects = $userObjects | Where-Object { $_.accountEnabled }
-}
-
-"## Inactive Users (No SignIn since at least $Days days.)"
-""
-$userObjects | Where-Object { $_.userType -eq "Member" } | Select-Object -Property UserPrincipalName, signInSessionsValidFromDateTime, accountEnabled | Sort-Object -Property signInSessionsValidFromDateTime | Format-Table UserPrincipalName,@{L=’Last Signin’;E={$_.signInSessionsValidFromDateTime}},@{L=’Account Enabled’;E={$_.accountEnabled}} | Out-String
-""
-
-"## Inactive Guests (No SignIn since at least $Days days.)"
-""
-$userObjects | Where-Object { $_.userType -eq "Guest" } | Select-Object -Property Mail, signInSessionsValidFromDateTime, accountEnabled | Sort-Object -Property signInSessionsValidFromDateTime | Format-Table Mail,@{L=’Last Signin’;E={$_.signInSessionsValidFromDateTime}},@{L=’Account Enabled’;E={$_.accountEnabled}} | Out-String
+Invoke-RjRbRestMethodGraph -Resource "/roleManagement/directory/roleAssignmentScheduleInstances" -OdFilter $filter
