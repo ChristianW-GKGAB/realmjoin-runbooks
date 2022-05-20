75e9ac47ec2cb6d2d904eafa8d20ac896b085b7d
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-05-20 13:15:24 +0200

    added output for nothing found

--- org/security/list-expiring-role-assignments.ps1
+++ org/security/list-expiring-role-assignments.ps1
@@ -1,9 +1,9 @@
 <#
   .SYNOPSIS
-  List users, that have no recent signins.
+  List Azure AD role assignments that will expire before a given number of days.
 
   .DESCRIPTION
-  List users, that have no recent signins.
+  List Azure AD role assignments that will expire before a given number of days.
 
   .NOTES
   Permissions: MS Graph
@@ -25,8 +25,6 @@
 param(
   [ValidateScript( { Use-RJInterface -DisplayName "Days without signin" } )]
   [int] $Days = 3000,
-  [ValidateScript( { Use-RJInterface -DisplayName "Include users/guests that can not sign in" } )]
-  [bool] $showBlockedUsers = $true,
   # CallerName is tracked purely for auditing purposes
   [Parameter(Mandatory = $true)]
   [string] $CallerName
@@ -37,10 +35,14 @@ Connect-RjRbGraph
 # Calculate "last sign in date"
 $expiringDate = (get-date).AddDays($Days) | Get-Date -Format "yyyy-MM-dd"
 $filter = "EndDateTime lt $expiringDate" + "T00:00:00Z"
-
+"## shows the non role assignments that will expire before $expiringDate"
 $roleassignments =  Invoke-RjRbRestMethodGraph -Resource "/roleManagement/directory/roleAssignmentScheduleInstances" -OdFilter $filter
 foreach ($roleassignment in $roleassignments) {
   $roleName = (Invoke-RjRbRestMethodGraph -Resource "/roleManagement/directory/roleDefinitions/$($roleassignment.roleDefinitionId)").DisplayName
   $roleassignment | Add-Member -Name "DisplayName" -Value $roleName -MemberType "NoteProperty"
 }
-$roleassignments
+if ($roleassignments){
+  $roleassignments
+}else {
+  "## no role assignments will expire in the next $Days Days"
+}
