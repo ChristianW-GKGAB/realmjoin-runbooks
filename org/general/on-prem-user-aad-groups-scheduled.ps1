0653f40c9149550506af9795b933228017296fdb
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-06-02 08:55:02 +0200

    add schedule possibility

---
+++ org/general/on-prem-user-aad-groups-scheduled.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+add on prem Users to a AAD Groups.
+
+#>
+
+
+[string] $MigGroupName = ""
+[string] $AAdGroupIDs = ""
+
+Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
+
+$beforedate = (Get-Date).AddDays(-1)
+$AADGroupIDArray =  $AAdGroupIDs.Split(',')
+$AllUsers = Get-ADUser -Properties whencreated -Filter *
+
+$AADGroups = @()
+foreach($AADGroupID in $AADGroupIDArray){
+    $AADGroups += Get-MgGroup -GroupId $AADGroupID
+}
+
+$MigGroupMemberGUIDs = get-ADGroupMember -Identity $MigGroupName -Recursive | Select-Object -ExpandProperty ObjectGUID
+$NewUsers = @()
+foreach($User in $AllUsers){
+    if(($User.whencreated -gt $beforedate) -or ($MigGroupMemberGUIDs.Contains($User.ObjectGUID))){
+        $NewUsers += $User
+    }
+}
+$AllAADUsers = Get-MgUser -Count userCount -ConsistencyLevel eventual -Property onPremisesUserPrincipalName,Id,userPrincipalName
+$AADUsers = @()
+foreach ($NewUser in $NewUsers){
+   $AADUsers += $AllAADUsers | Where-Object {$_.OnPremisesUserPrincipalName -eq $NewUser.UserPrincipalName }
+}
+foreach($AADGroup in $AADGroups){
+    
+    $AADGroupMembers = @()
+    $AADGroupMembers += Get-MgGroupMember -GroupId $AADGroup.id | Select-Object -ExpandProperty id
+
+    foreach($AADUser in $AADUsers){
+        if(!($AADgroupMembers.contains($AADUser.id))){
+            New-MgGroupMember -GroupId $AADGroup.id -DirectoryObjectId $AADUser.id
+            "## added $($AADUser.UserPrincipalName) to $($AADGroup.DisplayName)"
+        }
+    }
+    
+}
