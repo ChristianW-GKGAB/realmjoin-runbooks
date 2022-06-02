9a31e19d04289c63d3b206e61404fb788d34003c
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-06-02 09:31:25 +0200

    add params

--- org/general/onprem-change-autopilotGroupTags.ps1
+++ org/general/onprem-change-autopilotGroupTags.ps1
@@ -1,3 +1,39 @@
+<#
+.SYNOPSIS
+Assign an Autopilot GroupTag to a comma separated list of serial numbers (devices).
+
+.PARAMETER serialNumberList
+Comma separated list of serial numbers. Alternatively use 'inputFile'.
+
+.PARAMETER inputFile
+A file, containing a comma separated list of serial numbers (devices).
+
+.PARAMETER groupTag
+Autopilot GroupTag to assign to the devices.
+
+#>
+
+param(
+    [string] $serialNumberList = "",
+    [string] $inputFile = "",
+    [Parameter(Mandatory = $true)]
+    [string] $groupTag = ""
+)
+
+if ((-not $serialNumberList) -and (-not $inputFile)) {
+    "Please use either '-serialNumberList' or '-inputFile' to give a list serial numbers."
+    exit
+}
+
+if (-not $serialNumberList) {
+    if (-not (Test-Path -Path $inputFile -PathType Container)) {
+        "'$inputFile' not found."
+        exit
+    }
+    $serialNumberList = get-content -Raw -Path $inputFile
+}
+
+# Connecting
 Connect-MSGraph
 Update-MSGraphEnvironment -SchemaVersion "Beta" -Quiet
 Connect-MSGraph -Quiet
@@ -5,14 +41,10 @@ Connect-MSGraph -Quiet
 # Get all autopilot devices (even if more than 1000)
 #$autopilotDevices = Invoke-MSGraphRequest -HttpMethod GET -Url "deviceManagement/windowsAutopilotDeviceIdentities" | Get-MSGraphAllPages
 
-$serialnumberlist = ""
-$serialnumbers = $serialnumberlist.Split(',')
-$groupTag = ""
+$serialnumbers = $serialNumberList.Split(',')
 
-foreach($serialnumber in $serialnumbers){
-
-
-    $requestBody=
+foreach ($serialnumber in $serialnumbers) {
+    $requestBody =
 @"
     {
         groupTag: `"$groupTag`",
