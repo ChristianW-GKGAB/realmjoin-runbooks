e3bec8250fa4a90529170c9bdf7ceb9a9627e1ef
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-06-02 08:45:04 +0200

    fixed typo in if statement

--- org/general/on-prem-user-aad-groups.ps1
+++ org/general/on-prem-user-aad-groups.ps1
@@ -19,21 +19,20 @@ param(
     [Parameter(Mandatory = $true)]
     [string] $MigGroupName = ""
 )
-$AADGroupIDArray =  $AAdGroupIDs.Split(',')
+
 
 Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
 
+$beforedate = (Get-Date).AddDays(-1)
+$AADGroupIDArray =  $AAdGroupIDs.Split(',')
+$AllUsers = Get-ADUser -Properties whencreated -Filter *
 
 $AADGroups = @()
 foreach($AADGroupID in $AADGroupIDArray){
-    $AADGroups += Get-MgGroupMember -GroupId $AADGroupID
+    $AADGroups += Get-MgGroup -GroupId $AADGroupID
 }
 
-$AllUsers = Get-ADUser -Properties whencreated -Filter *
-$beforedate = (Get-Date).AddDays(-1)
-
 $MigGroupMemberGUIDs = get-ADGroupMember -Identity $MigGroupName -Recursive | Select-Object -ExpandProperty ObjectGUID
-$MigGroupMemberGUIDs
 $NewUsers = @()
 foreach($User in $AllUsers){
     if(($User.whencreated -gt $beforedate) -or ($MigGroupMemberGUIDs.Contains($User.ObjectGUID))){
@@ -44,13 +43,14 @@ $AllAADUsers = Get-MgUser -Count userCount -ConsistencyLevel eventual -Property 
 $AADUsers = @()
 foreach ($NewUser in $NewUsers){
    $AADUsers += $AllAADUsers | Where-Object {$_.OnPremisesUserPrincipalName -eq $NewUser.UserPrincipalName }
-   "## added $($NewUser.userPrincipalName) to AAD Users"
 }
 foreach($AADGroup in $AADGroups){
+    
     $AADGroupMembers = @()
-    $AADGroupMembers += Get-MgGroupMember -Identity $AADGroup.id -Recursive | Select-Object -ExpandProperty id
+    $AADGroupMembers += Get-MgGroupMember -GroupId $AADGroup.id | Select-Object -ExpandProperty id
+
     foreach($AADUser in $AADUsers){
-        if(!($ADgroupMembers.contains($AADUser.id))){
+        if(!($AADgroupMembers.contains($AADUser.id))){
             New-MgGroupMember -GroupId $AADGroup.id -DirectoryObjectId $AADUser.id
             "## added $($AADUser.UserPrincipalName) to $($AADGroup.DisplayName)"
         }
