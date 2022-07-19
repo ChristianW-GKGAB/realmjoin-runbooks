a28bc76b5d8c2cc4eea0b2fa9900fc12d62cda25
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-07-19 13:29:49 +0200

    add additional functionalities

--- org/security/on-prem-export-abandoned-devices.ps1
+++ org/security/on-prem-export-abandoned-devices.ps1
@@ -8,24 +8,43 @@ import-module ActiveDirectory
 #Disable abandoned devices
 [bool] $Disable = $false
 #export the list of Abandoned Devices in a .csv
-[bool] $exportcsv = $false
+[bool] $exportcsv = $true
 if($exportcsv){
     #csv Path
-    [string] $CsvPath = 
-    [string] $csvName = "AbandonedDevices" + (get-date).ToString("yyyymmdd")
+    [string] $CsvPath = ""
+    [string] $csvName = "AbandonedDevices" + (get-date).ToString("yyyyMMddhhmm")
 }
+#included OU (Organisational Units
+[array] $IncludedOUs = ""
 #Excluded OU (Organisational Units) 
 [array] $ExcludedOUs = "OUAllowOldPasswords","Domain Controllers"
+#excluded Devices
+[array] $ExcludedDevices = ""
 # CallerName is tracked purely for auditing purposes
 [string] $CallerName
 
 
 $date = [datetime]::Today.AddDays(-$Days)
 
-$AbandonedDevices = Get-ADComputer -Filter 'PasswordLastSet -le $date' -Properties "PasswordLastSet", "DistinguishedName" 
+$AllAbandonedDevices = Get-ADComputer -Filter 'PasswordLastSet -le $date' -Properties "PasswordLastSet", "DistinguishedName" | Select-Object -Property DistinguishedName, DNSHostName, Enabled, ObjectGUID, PasswordLastSet
+
+$AbandonedDevices = @()
+if($IncludedOUs.Length -ge 1 -and $IncludedOUs[0] -ne ""){
+    foreach($IncludedOU in $IncludedOUs){
+       $AbandonedDevices += $AllAbandonedDevices | Where-Object {$_.DistinguishedName -like "*OU=$($includedOU),*"}
+    }
+}
+else{
+    $AbandonedDevices = $AllAbandonedDevices
+}
 foreach($ExcludedOU in $ExcludedOUs){
     $AbandonedDevices = $AbandonedDevices | Where-Object {$_.DistinguishedName -notlike "*OU=$($ExcludedOU),*"}
 }
+
+foreach($ExcludedDevice in $ExcludedDevices){
+    $AbandonedDevices = $AbandonedDevices | Where-Object {$_.DistinguishedName -ne $ExcludedDevices}
+}
+
 
 if($Delete){
     $AbandonedDevices | Remove-ADComputer
@@ -34,7 +53,8 @@ if($Delete){
         set-AdComputer -Identity $AbandonedDevice -Enabled $false
     }
 }elseif($exportcsv){
-    $AbandonedDevices | Export-Csv -Path "$($CsvPath)$($csvName).csv"
+    $AbandonedDevices | Export-Csv -Path "$($CsvPath)$($csvName).csv" -NoTypeInformation
+    $AbandonedDevices
 }else{
     $AbandonedDevices
 }
