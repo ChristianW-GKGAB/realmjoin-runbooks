4dd4bc3a0751bb761bc92a03ce323a86db392359
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-07-27 09:35:56 +0200

    add remove user from group functionality

--- org/general/on-prem-user-aad-groups-scheduled.ps1
+++ org/general/on-prem-user-aad-groups-scheduled.ps1
@@ -1,51 +1,67 @@
 <#
 .SYNOPSIS
 add on prem Users to a AAD Groups.
-
+remove on prem Users from AAD Groups.
 #>
 
 
 [string] $MigGroupName = ""
 [string] $AAdGroupIDs = ""
+[string] $RemoveAADGroupIDs = ""
 [string] $logsFolder = ""
 
-Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
+Connect-MgGraph -Scopes "User.Read.All", "Group.ReadWrite.All"
 
 $beforedate = (Get-Date).AddDays(-1)
-$AADGroupIDArray =  $AAdGroupIDs.Split(',')
+$AADGroupIDArray = $AAdGroupIDs.Split(',')
+$RemoveAADGroupIDArray = $RemoveAADGroupIDs.Split(',')
 $AllUsers = Get-ADUser -Properties whencreated -Filter *
 
 $AADGroups = @()
-foreach($AADGroupID in $AADGroupIDArray){
+foreach ($AADGroupID in $AADGroupIDArray) {
     $AADGroups += Get-MgGroup -GroupId $AADGroupID
+}
+$removeAADGroups = @()
+foreach ($RemoveAADGroupID in $RemoveAADGroupIDArray) {
+    $removeAADGroups += Get-MgGroup -GroupId $RemoveAADGroupID
 }
 
 $MigGroupMemberGUIDs = get-ADGroupMember -Identity $MigGroupName -Recursive | Select-Object -ExpandProperty ObjectGUID
 $NewUsers = @()
 $logs = @()
-foreach($User in $AllUsers){
-    if(($User.whencreated -gt $beforedate) -or ($MigGroupMemberGUIDs.Contains($User.ObjectGUID))){
+foreach ($User in $AllUsers) {
+    if (($User.whencreated -gt $beforedate) -or ($MigGroupMemberGUIDs.Contains($User.ObjectGUID))) {
         $NewUsers += $User
     }
 }
-$AllAADUsers = Get-MgUser -Count userCount -ConsistencyLevel eventual -Property onPremisesUserPrincipalName,Id,userPrincipalName
+$AllAADUsers = Get-MgUser -Count userCount -ConsistencyLevel eventual -Property onPremisesUserPrincipalName, Id, userPrincipalName
 $AADUsers = @()
-foreach ($NewUser in $NewUsers){
-   $AADUsers += $AllAADUsers | Where-Object {$_.OnPremisesUserPrincipalName -eq $NewUser.UserPrincipalName }
+foreach ($NewUser in $NewUsers) {
+    $AADUsers += $AllAADUsers | Where-Object { $_.OnPremisesUserPrincipalName -eq $NewUser.UserPrincipalName }
 }
 
-foreach($AADGroup in $AADGroups){
+foreach ($AADGroup in $AADGroups) {
     
     $AADGroupMembers = @()
     $AADGroupMembers += Get-MgGroupMember -GroupId $AADGroup.id | Select-Object -ExpandProperty id
 
-    foreach($AADUser in $AADUsers){
-        if(!($AADgroupMembers.contains($AADUser.id))){
+    foreach ($AADUser in $AADUsers) {
+        if (!($AADgroupMembers.contains($AADUser.id))) {
             New-MgGroupMember -GroupId $AADGroup.id -DirectoryObjectId $AADUser.id
             $logs += "## added $($AADUser.UserPrincipalName) to $($AADGroup.DisplayName)"
         }
     }
     
+}
+foreach ($RemoveAADGroup in $RemoveAADGroups) {
+    $RemoveAADGroupMembers = @()
+    $RemoveAADGroupMembers = Get-MgGroupMember -GroupId $RemoveAADGroup.id | Select-Object -ExpandProperty id
+    foreach ($AADUser in $AADUsers) {
+        if ($RemoveAADGroupMembers.contains($AADUser.id)) {
+            Remove-MgGroupMemberByRef -GroupId $RemoveAADGroup.id -DirectoryObjectId $AADUser.id
+            $logs += "## removed $($AADUser.UserPrincipalName) from $($RemoveAADGroup.DisplayName)"
+        }
+    }
 }
 $time = get-date -Format "yyyyMMddTHH"
 $logs | Out-File "$logsFolder\logs-$time.txt"
@@ -53,7 +69,7 @@ $logs | Out-File "$logsFolder\logs-$time.txt"
 
 #Delete files older than 1 month
 Get-ChildItem $logsFolder -Recurse -Force -ea 0 |
-Where-Object {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-30)} |
+Where-Object { !$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-30) } |
 ForEach-Object {
-   $_ | Remove-Item -Force
+    $_ | Remove-Item -Force
 }
