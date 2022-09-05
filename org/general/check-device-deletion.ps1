08b98ca01f594d190c9e292890d4de24e7cc3ea6
Christian Wallesch <88315147+ChristianW-GKGAB@users.noreply.github.com>
2022-09-05 15:01:59 +0200

    change filter and output

--- org/general/check-device-deletion.ps1
+++ org/general/check-device-deletion.ps1
@@ -27,43 +27,67 @@ param(
     [ValidateScript( { Use-RJInterface -DisplayName "Serial Number of the Device" } )]
     [Parameter(Mandatory = $true)]
     [string] $SerialNumber,
-    [ValidateScript( { Use-RJInterface -DisplayName "Disable or Enable Device" } )]
-    [bool] $Enable = $false,
     # CallerName is tracked purely for auditing purposes
     [Parameter(Mandatory = $true)]
     [string] $CallerName
 )
 
-Connect-RjRbGraph
+Connect-RjRbGraph 
 
 
 
-$deviceIntune = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices" -OdFilter "serialNumber eq '$($SerialNumber)'"
-$autopilotdevice = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities" -OdFilter "serialNumber eq '$($SerialNumber)'"
+$deviceIntune = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices" -OdFilter "serialNumber eq '$($SerialNumber)'" -ErrorAction SilentlyContinue
+$autopilotdevice = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities" -OdFilter "contains(serialNumber,'$($SerialNumber)')" -ErrorAction SilentlyContinue 
+
+
 
 #$deviceAAD = Invoke-RjRbRestMethodGraph -Resource "/devices"
 
 
 
-if($autopilotdevice){
-    "## autopilot true"
+if($autopilotdevice -and $deviceIntune){
+    $AADdevice = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "deviceId eq '$($deviceIntune.azureADDeviceId)'"
+    "## Autopilot: exists"
+    "## Intune: exists"
+    if($AADdevice){
+        "## Azure AD: exists"
+    }
+    else{
+        "## Azure AD: does not exist"
+    }
+    "## Devicename: $($deviceIntune.deviceName)"
+    "## Model: $($deviceIntune.model)"
+    "## Manufacturer: $($deviceIntune.manufacturer)"
+}
+elseif (($null -eq $deviceIntune) -and $autopilotdevice) {
+    $AADdevice = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "deviceId eq '$($autopilotdevice.azureActiveDirectoryDeviceId)'" -ErrorAction SilentlyContinue
+    "## Autopilot: exists"
+    "## Intune: does not exist"
+    if($AADdevice){
+        "## Azure AD: exists"
+    }
+    else{
+        "## Azure AD: does not exist"
+    }
+    "## Devicename: $($autopilotdevice.deviceName)"
+    "## Model: $($autopilotdevice.model)"
+    "## Manufacturer: $($autopilotdevice.manufacturer)"
+}
+elseif(($null -eq $autopilotdevice) -and $deviceIntune){
+    $AADdevice = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "deviceId eq '$($deviceIntune.azureADDeviceId)'" -ErrorAction SilentlyContinue
+    "## Autopilot: does not exist"
+    "## Intune: exists"
+    if($AADdevice){
+        "## Azure AD: exists"
+    }
+    else{
+        "## Azure AD: does not exist"
+    }
+    "## Devicename: $($deviceIntune.deviceName)"
+    "## Model: $($deviceIntune.model)"
+    "## Manufacturer: $($deviceIntune.manufacturer)"
 }
 else{
-    "## autopilot false"
+    "## Autopilot: does not exist"
+    "## Intune: does not exist"
 }
-
-if($deviceIntune){
-    "## intune true"
-    "## Devicename $($deviceIntune.deviceName)"
-    "## Model $($deviceIntune.model)"
-    "## Manufacturer $($deviceIntune.manufacturer)"
-}
-else{
-    "## intune false"
-}
-if(($null -eq $deviceIntune) -and $autopilotdevice){
-    "## Devicename $($autopilotdevice.deviceName)"
-    "## Model $($autopilotdevice.model)"
-    "## Manufacturer $($autopilotdevice.manufacturer)"
-}
-
